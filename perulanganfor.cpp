7d4fd44f1c3b99be163b0684c68975bc6861aaa3
HAFIDH AHMAD FAUZAN 2019 MI A <56226220+hafidh2001@users.noreply.github.com>
2019-10-07 21:54:28 +0700

    Add files via upload

---
+++ perulanganfor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std ;
+
+int main () {
+	
+	system ("title HAFIDH AHMAD FAUZAN") ;
+	
+	cout << "NAMA  : HAFIDH AHMAD FAUZAN\n" ;
+	cout << "NIM   : 19051397027\n" ;
+	cout << "PRODI : D4 MANAJEMEN INFORMATIKA\n" ;
+	cout << "\n\n" ;
+	
+	int a, b , jumlah ;
+	
+	cout << "Masukkan angka = " ;
+	cin >> b ;
+	
+	for (a=1 ; b<=10 ; b++) {
+		jumlah = a*b ;
+		cout << jumlah << endl ; ;
+	}
+	
+	return 0 ;
+	
+}
